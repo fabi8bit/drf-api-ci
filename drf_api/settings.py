2bcd0bd3bd97298ff380569374fc27e42f341de2
fabi8bit <fabi8bit@gmail.com>
2023-08-31 09:28:44 +0000

    Fixed typos on settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -56,7 +56,7 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAIL_SERIALIZERS': 'drf_api.serializers.CurrentUserSerializer' 
+    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer' 
 }
 
 
