db7c3c6769c5dfba821569ed512319dc8fc5b399
fabi8bit <fabi8bit@gmail.com>
2023-09-11 09:01:54 +0000

    Fix CLIENT_ORIGIN on settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -126,6 +126,12 @@ MIDDLEWARE = [
 #     CORS_ALLOWED_ORIGIN_REGEXES = [
 #         r"^https://.*\.gitpod\.io$",
 #     ]
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+    os.environ.get('CLIENT_ORIGIN'),
+    os.environ.get('CLIENT_ORIGIN_DEV')
+]
+
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
